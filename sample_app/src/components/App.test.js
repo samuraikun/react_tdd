fc9f517113d04740643685f264d4b8e27c9f294a
samuraikun <tazitaziawawa@gmail.com>
2018-10-13 16:03:43 +0900

    refactor test

--- sample_app/src/components/App.test.js
+++ sample_app/src/components/App.test.js
@@ -2,18 +2,32 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-const app = shallow(<App />);
+describe('App', () => {
+  const app = shallow(<App />);
 
-it('renders correctly', () => {
-  expect(app).toMatchSnapshot();
-});
+  it('renders correctly', () => {
+    expect(app).toMatchSnapshot();
+  });
 
-it('initializes the `state` with an empty list of gists', () => {
-  expect(app.state().gifts).toEqual([]);
-});
+  it('initializes the `state` with an empty list of gists', () => {
+    expect(app.state().gifts).toEqual([]);
+  });
 
-it('add a new gift to `state` when clicked the `add gift` button', () => {
-  app.find('.btn-add').simulate('click');
+  describe('when clicked the `お布施する` button', () => {
+    beforeEach(() => {
+      app.find('.btn-add').simulate('click');
+    });
 
-  expect(app.state().gifts).toEqual([{id: 1}]);
+    afterEach(() => {
+      app.setState({ gifts: [] });
+    });
+
+    it('add a new gift to `state` when clicked the `お布施する` button', () => {
+      expect(app.state().gifts).toEqual([{id: 1}]);
+    });
+  
+    it('add a new gift to the rendered list when clicking the `お布施する` button', () => {
+      expect(app.find('.gift-list').children().length).toEqual(1);
+    });
+  })
 });
