d615a0d43b2deee9d328f13d483ec8d9c5c22b67
samuraikun <tazitaziawawa@gmail.com>
2018-10-14 15:06:53 +0900

    入金・支払い機能の実装とテストを追加

--- wallet/src/reducers/balance.test.js
+++ wallet/src/reducers/balance.test.js
@@ -1,11 +1,36 @@
 import balanceReducer from './balance';
+import balanceReducer2 from './balance';
 import * as constants from '../actions/constants';
 
 describe('balanceReducer', () => {
-  it('sets a balance', () => {
+  describe('when initializing', () => {
     const balance = 10;
 
-    expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance }))
-      .toEqual(balance);
+    it('sets a balance', () => {
+      expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance }))
+        .toEqual(balance);
+    });
+
+    describe('then re-initializing', () => {
+      it('reads the balance from cookies', () => {
+        expect(balanceReducer2(undefined, {})).toEqual(balance);
+      });
+    });
   });
-})
+
+  it('deposits into the balance', () => {
+    const deposit = 10;
+    const initialState = 5;
+
+    expect(balanceReducer(initialState, { type: constants.DEPOSIT, deposit}))
+      .toEqual(initialState + deposit);
+  });
+
+  it('withdraw from the balance', () => {
+    const withdrawal = 10;
+    const initialState = 20;
+
+    expect(balanceReducer(initialState, { type: constants.WITHDRAW, withdrawal }))
+      .toEqual(initialState - withdrawal);
+  });
+});
