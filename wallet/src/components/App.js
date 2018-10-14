ac7449aac46ebcdd64f1cf5399ce7113c0b9f164
samuraikun <tazitaziawawa@gmail.com>
2018-10-14 19:06:21 +0900

    App コンポーネントをSFC化

--- wallet/src/components/App.js
+++ wallet/src/components/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Wallet from './Wallet';
 import Bitcoin from './Bitcoin';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Wallet Check</h2>
-        <hr />
-        <Wallet />
-        <hr />
-        <Bitcoin />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <h2>Wallet Check</h2>
+      <hr />
+      <Wallet />
+      <hr />
+      <Bitcoin />
+    </div>
+  );
 }
 
 export default App;
